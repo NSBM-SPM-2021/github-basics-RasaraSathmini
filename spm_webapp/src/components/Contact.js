763e4f0f0c51fe86c0df8fcd6a1803f1fdd6c1b2
Rasara Sathmini <56917758+RasaraSathmini@users.noreply.github.com>
2021-09-11 14:29:25 +0530

    frontend_StudentRegisterForm

---
+++ spm_webapp/src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+import firebaseDb from "../firebasedb";
+
+const Contacts = () => {
+  const addorEdit = (obj) => {
+    firebaseDb.child("contacts").push(obj, (err) => {
+      if (err) console.log(err);
+    });
+  };
+
+  return (
+    <>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4 text-center">Student Register</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-5">
+          <ContactForm addorEdit={addorEdit} />
+        </div>
+        <div className="col-md-7">
+          <div> list of contacts </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contacts;
