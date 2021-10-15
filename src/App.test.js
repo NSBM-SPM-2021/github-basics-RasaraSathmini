dadecd84695b024cb98f3f6c35926c45d815e9c2
Rasara Sathmini <56917758+RasaraSathmini@users.noreply.github.com>
2021-10-15 14:06:03 +0530

    UnitTests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Contact from './components/Contact';
+import ContactForm from './components/ContactForm'; 
+
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<Contact/>);
+  expect(wrapper.find("h1").text()).toContain("Student Register");
+
+  //const {getByText} = render(<Contact />);
+  //const linkElement = getByText("Student Register");
+  //expect(linkElement).toBeInTheDocument();
+});
+
+test('render a button', () => {
+  const wrapper = shallow(<ContactForm/>);
+  expect(wrapper.find("#submit"));
 });
